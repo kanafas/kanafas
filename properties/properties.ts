da1f6a1139d4882da14f3a5e367672a8c9b54977
Adam Josefus <adamjosefus@gmail.com>
2020-07-05 20:03:28 +0200

    Update version

--- properties/properties.ts
+++ properties/properties.ts
@@ -1,12 +1,12 @@
 import { Fill } from './Fill.js';
-import { Font } from './Font.js';
+import { Font, FontFeatures } from './Font.js';
 import { Shadow } from './Shadow.js';
 import { Stroke } from './Stroke.js';
 import { Transform } from './Transform.js';
 
 
 export { Fill };
-export { Font };
+export { Font, FontFeatures };
 export { Shadow };
 export { Stroke };
 export { Transform };
