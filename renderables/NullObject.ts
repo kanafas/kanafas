b3ab59831306cf0d1c9b9e3b462ba438469442f4
Adam Josefus <adamjosefus@gmail.com>
2020-10-16 23:21:27 +0200

    Implement IClonable to NullObject

--- renderables/NullObject.ts
+++ renderables/NullObject.ts
@@ -4,15 +4,15 @@ import { Transform } from "../properties/Transform.js";
 import { Vector } from "../units/Vector.js";
 import { IRenderingLayer } from "../core/RenderingLayer.js";
 import { Gizmo } from "../debugger/Gizmo.js";
+import { IClonable } from "../core/IClonable.js";
 
 
-export class NullObject implements IObject {
+export class NullObject implements IObject, IClonable<NullObject> {
 
     transform: Transform = new Transform();
 
 
-    constructor() {
-    }
+    constructor() { }
 
 
     getBoundingBox(renderingLayer: IRenderingLayer): IBoundingBox {
@@ -32,5 +32,13 @@ export class NullObject implements IObject {
         renderingLayer.setMatrixToTransform(this.transform);
         Gizmo.origin(renderingLayer, Vector.zero(), Gizmo.nullColor);
         renderingLayer.resetMatrix();
+    }
+
+
+    clone(): NullObject {
+        const n = new NullObject();
+        n.transform = this.transform.clone();
+
+        return n;
     }
 }
