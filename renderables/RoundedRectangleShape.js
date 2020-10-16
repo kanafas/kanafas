5ebc9b67522f494bd514455db1a4b957b8d6063b
Adam Josefus <adamjosefus@gmail.com>
2020-10-16 12:47:28 +0200

    Implement IClonable to RoundedRectangleShape

--- renderables/RoundedRectangleShape.js
+++ renderables/RoundedRectangleShape.js
@@ -14,4 +14,16 @@ export class RoundedRectangleShape extends RoundedRectangleGeometry {
     renderGizmo(renderingLayer) {
         Shape.renderGizmo(renderingLayer, this);
     }
+    clone() {
+        const topLeftRadius = { x: this.topLeftRadius.x, y: this.topLeftRadius.y };
+        const topRightRadius = { x: this.topRightRadius.x, y: this.topRightRadius.y };
+        const bottomRightRadius = { x: this.bottomRightRadius.x, y: this.bottomRightRadius.y };
+        const bottomLeftRadius = { x: this.bottomLeftRadius.x, y: this.bottomLeftRadius.y };
+        const shape = new RoundedRectangleShape(this.width, this.height, topLeftRadius, topRightRadius, bottomRightRadius, bottomLeftRadius);
+        shape.fill = this.fill?.clone() ?? null;
+        shape.stroke = this.stroke?.clone() ?? null;
+        shape.shadow = this.shadow?.clone() ?? null;
+        shape.opacity = this.opacity;
+        return shape;
+    }
 }
--- renderables/RoundedRectangleShape.ts
+++ renderables/RoundedRectangleShape.ts
@@ -6,15 +6,16 @@ import { Shadow } from "./../properties/Shadow.js";
 import { IRenderingLayer } from "./../core/RenderingLayer.js";
 import { Shape } from "./Shape.js";
 import { MultipleRadiusInitType, RoundedRectangleGeometry } from "./RoundedRectangleGeometry.js";
+import { IClonable } from "../core/IClonable.js";
 
 
-export class RoundedRectangleShape extends RoundedRectangleGeometry implements IRenderable, IShape {
+export class RoundedRectangleShape extends RoundedRectangleGeometry implements IRenderable, IShape, IClonable<RoundedRectangleShape> {
 
     fill: Fill | null = null;
     stroke: Stroke | null = null;
     shadow: Shadow | null = null;
-
     opacity: number = 1;
+
 
     constructor(width: number, height: number, ...radius: MultipleRadiusInitType) {
         super(width, height, ...radius);
@@ -28,5 +29,21 @@ export class RoundedRectangleShape extends RoundedRectangleGeometry implements I
 
     renderGizmo(renderingLayer: IRenderingLayer) {
         Shape.renderGizmo(renderingLayer, this);
+    }
+
+
+    clone(): RoundedRectangleShape {
+        const topLeftRadius = { x: this.topLeftRadius.x, y: this.topLeftRadius.y };
+        const topRightRadius = { x: this.topRightRadius.x, y: this.topRightRadius.y };
+        const bottomRightRadius = { x: this.bottomRightRadius.x, y: this.bottomRightRadius.y };
+        const bottomLeftRadius = { x: this.bottomLeftRadius.x, y: this.bottomLeftRadius.y };
+
+        const shape = new RoundedRectangleShape(this.width, this.height, topLeftRadius, topRightRadius, bottomRightRadius, bottomLeftRadius);
+        shape.fill = this.fill?.clone() ?? null;
+        shape.stroke = this.stroke?.clone() ?? null;
+        shape.shadow = this.shadow?.clone() ?? null;
+        shape.opacity = this.opacity;
+
+        return shape;
     }
 }
